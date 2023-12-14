709d2d1a7f95fb5d6af387e6c5a6141f5ec20786
Alireza Hatami <itshatami@gmail.com>
2023-12-15 01:58:52 +0330

    data types

---
+++ DataType.dart
@@ -0,0 +1,24 @@
+void main() {
+  // DataTypes
+  // Strings , Numbers , Booleans , Lists , Maps , Dynamic
+
+  // String
+  String address = 'iran/golestan/gonbad';
+  print("String: $address");
+
+  // Integers
+  int mobile = 9398482612;
+  print("Integer: $mobile");
+
+  // Double
+  double price = 99.99;
+  print("Double: $price");
+
+  // Boolean
+  bool isActive = false;
+  print("Boolean: $isActive");
+
+  // Dynamic
+  dynamic id = "i dont know";
+  print("Dynamic $id");
+}
