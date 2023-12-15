abd607241ac2c4396dac51e304c9034c099267e0
Alireza Hatami <itshatami@gmail.com>
2023-12-15 18:13:24 +0330

    logic in Dart

---
+++ logic.dart
@@ -0,0 +1,11 @@
+void main() {
+  // Logic - If , If/Else , If Else If
+  var num = 7;
+  if (num == 7) {
+    print('the number is 7');
+  } else if (num == 3) {
+    print('the number is 3');
+  } else {
+    print('the number is NOT 7 or 3');
+  }
+}
