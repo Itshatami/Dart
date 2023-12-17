943ec6401d7a87391d2a5116cb722c4fd3d842a4
Alireza Hatami <itshatami@gmail.com>
2023-12-17 17:29:01 +0330

    dart user input

---
+++ input.dart
@@ -0,0 +1,9 @@
+import 'dart:io';
+
+void main() {
+  // User Inputs
+  print('Enter Your Name:');
+  // var name = stdin.readLineSync();
+  // String? name = stdin.readLineSync();
+  // print('Hello $name');
+}
